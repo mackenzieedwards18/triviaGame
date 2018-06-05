5cf033e2095e14e72aa9a247e9573a49f4d69d5f
mackenzieedwards18 <mackenzieedwards1995@gmail.com>
2018-06-05 12:42:06 -0700

    Finished html, css, still working on Javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+var count = 60;
+
+var correctCount = 0;
+var wrongCount = 0;
+var unansweredCount = 0;
+
+$(document).ready(function(){
+
+    $("#endContainer").hide();
+    
+
+})
+
+function countDown(){
+
+    count--;
+
+    $
+}
+
